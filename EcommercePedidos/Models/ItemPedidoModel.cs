6620992ec392c5ab9976d769dd3e2a75906b1fc1
garra400 <joaovicgomes.10@gmail.com>
2023-05-16 20:11:26 -0300

    changed non-null variables

--- EcommercePedidos/Models/ItemPedidoModel.cs
+++ EcommercePedidos/Models/ItemPedidoModel.cs
@@ -3,8 +3,8 @@ namespace EcommercePedidos.Models;
 public class ItemPedido
 {
     public int ItemPedidoID { get; set; }
-    private Produto _produto { get; set; }
+    private Produto? _produto { get; set; }
     private int _quantidade { get; set; }
     private decimal _precoUnitario { get; set; }
-    private Pedido _pedido { get; set; }
+    private Pedido? _pedido { get; set; }
 }
--- EcommercePedidos/Models/PedidoModel.cs
+++ EcommercePedidos/Models/PedidoModel.cs
@@ -3,6 +3,6 @@ public class Pedido
 {
     public int PedidoID { get; set; }
     private DateTime _dataPedido { get; set; }
-    private string _cliente { get; set; }
-    private string _status { get; set; }
+    private string? _cliente { get; set; }
+    private string? _status { get; set; }
 }
